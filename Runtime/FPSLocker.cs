0132e9483aa823e794f98aecc37dc9ba21e7e9e4
Benedek Mészáros <149479346+benedekmeszaros@users.noreply.github.com>
2024-03-07 10:24:04 +0100

    Add files via upload

---
+++ Runtime/FPSLocker.cs
@@ -0,0 +1,34 @@
+/*
+  _    _                     _                                  _                 _   _         
+ | |  | |                   | |                                | |               | | (_)        
+ | |__| |   ___  __  __   __| |   ___     __ _   ______   ___  | |_   _   _    __| |  _    ___  
+ |  __  |  / _ \ \ \/ /  / _` |  / _ \   / _` | |______| / __| | __| | | | |  / _` | | |  / _ \ 
+ | |  | | |  __/  >  <  | (_| | | (_) | | (_| |          \__ \ | |_  | |_| | | (_| | | | | (_) |
+ |_|  |_|  \___| /_/\_\  \__,_|  \___/   \__, |          |___/  \__|  \__,_|  \__,_| |_|  \___/ 
+                                          __/ |                                                 
+                                         |___/             
+*/
+using UnityEngine;
+
+namespace HexTools.Performance
+{
+    public class FPSLocker : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [SerializeField] private bool auto = true;
+        [SerializeField] private int cap = 60;
+#endif
+        private void Awake()
+        {
+#if UNITY_EDITOR
+            if (auto)
+                Application.targetFrameRate = Screen.currentResolution.refreshRate;
+            else
+                Application.targetFrameRate = cap;
+#else
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
+#endif
+        }
+    }
+}
+
